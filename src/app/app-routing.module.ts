fd36cf6a3a302cdc0cf5b5228773b67735f61b70
mfritts17 <85657644+mfritts17@users.noreply.github.com>
2022-10-10 12:23:24 -0400

    Added more

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
+import { getLocaleDayNames } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { ClockComponent } from './clock/clock.component';
 import { GamesComponent } from './games/games.component';
+import {TetrisComponent} from './games/tetris/tetris.component';
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 
 const routes: Routes = [
 {path:'clock',component : ClockComponent},
-{path: 'games',component : GamesComponent}
+{path:'games',component : GamesComponent},
+{path: 'games/tetris',component : TetrisComponent},
+{path:'list',component: ToDoListComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClockComponent } from './clock/clock.component';
+import { TetrisComponent } from './games/tetris/tetris.component';
 import { GamesComponent } from './games/games.component';
-
+import { ToDoListComponent } from './to-do-list/to-do-list.component';
 @NgModule({
   declarations: [
     AppComponent,
     ClockComponent,
-    GamesComponent
+    TetrisComponent,
+    GamesComponent,
+    ToDoListComponent
+    
   ],
   imports: [
     BrowserModule,
